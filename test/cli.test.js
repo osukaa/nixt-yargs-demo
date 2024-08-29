a6eac623e81f220420c424a76075f92435d04b14
Oscar Funes <571775+osukaa@users.noreply.github.com>
2024-08-28 21:44:32 -0700

    Incluide suite to wrap the tests

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,21 +1,23 @@
 'use strict';
 
 const path = require('node:path');
-const test = require('node:test');
+const { test, suite } = require('node:test');
 const nixt = require('nixt');
 
-test('command with name', (t, done) => {
-  nixt()
-    .cwd(path.join(__dirname, '..'))
-    .run('node cli.js hello --name=Oscar')
-    .stdout('Hello, Oscar!')
-    .end(done);
-});
+suite('hello command', () => {
+  test('command with name', (t, done) => {
+    nixt()
+      .cwd(path.join(__dirname, '..'))
+      .run('node cli.js hello --name=Oscar')
+      .stdout('Hello, Oscar!')
+      .end(done);
+  });
 
-test('command with default', (t, done) => {
-  nixt()
-    .cwd(path.join(__dirname, '..'))
-    .run('node cli.js hello')
-    .stdout('Hello, World!')
-    .end(done);
+  test('command with default', (t, done) => {
+    nixt()
+      .cwd(path.join(__dirname, '..'))
+      .run('node cli.js hello')
+      .stdout('Hello, World!')
+      .end(done);
+  });
 });
